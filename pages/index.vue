f53d61301a84262113fd8adbcbbb00bc983d6707
Henrik Fricke <henrik.fricke@jimdo.com>
2018-10-26 22:35:26 +0200

    Add Topbar component and integrate it in index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,32 @@
 <template>
-  <section class="container">
-    <ul>
-      <li
-        v-for="article in articles"
-        :key="article.id"
-      >
-        {{ article.headline }}
-      </li>
-    </ul>
-  </section>
+  <div>
+    <topbar :nav-items="navItems" />
+    <section class="flex justify-center">
+      <ul>
+        <li
+          v-for="article in articles"
+          :key="article.id"
+        >
+          {{ article.headline }}
+        </li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script>
+import Topbar from '~/components/Topbar'
 import { createArticleRepository } from '~/factories/createArticleRepository'
 
 export default {
+  components: {
+    Topbar
+  },
   async asyncData() {
     const repository = createArticleRepository()
     const articles = await repository.findAll()
 
-    return { articles }
+    return { articles, navItems: [{ url: '#', label: 'test' }] }
   }
 }
 </script>
-
-<style>
-.container {
-  @apply flex justify-center;
-}
-</style>
