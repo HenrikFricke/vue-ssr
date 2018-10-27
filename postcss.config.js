29cdbbdc77b12c078170d76dad7272aa9d13ccc2
Henrik Fricke <henrik.fricke@jimdo.com>
2018-10-27 03:13:39 +0200

    Minify css in production mode

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,17 @@
 const join = require('path').join
 const tailwindJS = join(__dirname, 'tailwind.js')
 
-module.exports = {
-  plugins: [
+module.exports = () => {
+  const plugins = [
     require('tailwindcss')(tailwindJS),
     require('autoprefixer')
   ]
+
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push(require('cssnano')({
+      preset: 'default',
+    }))
+  }
+
+  return { plugins }
 }
